30ac8c7707f5e5a5581d0bd046106e8de39e1642
clmoon <aruham.cp3@qq.com>
2019-04-12 17:35:29 +0800

    update seckill.js handleSeckill()

--- src/main/webapp/resources/script/seckill.js
+++ src/main/webapp/resources/script/seckill.js
@@ -6,9 +6,55 @@
 var seckill = {
     /*封装秒杀相关ajax的url*/
     URL: {
-    	now : function(){
+    	now: function(){
     		return '/seckill/time/now';
+    	},
+    	exposer: function(seckillId){
+    		return '/seckill/'+seckillId+'/exposer';
+    	},
+    	execution: function(seckillId,md5){
+    		return '/seckill/'+seckillId+'/'+md5+'/execution';
     	}
+    },
+    handleSeckill: function(seckillId,node){
+    	//获取秒杀地址，控制显示逻辑，执行秒杀
+    	node.hide().html('<button class="btn btn-primary btn-lg" id="killBtn">开始秒杀</button>');
+    	$.post(seckill.URL.exposer(seckillId),{},function(result){
+    		if(result && result['success']){
+    			var exposer = result['data'];
+    			if(exposer['exposed']){
+    				//开启秒杀
+    				//获取秒杀地址
+    				var md5 = exposer['md5'];
+    				var killUrl = seckill.URL.execution(seckillId,md5);
+    				$('#killBtn').one('click',function(){
+    					//执行秒杀请求
+    					//1.先禁用按钮
+    					$(this).addClass('disabled');
+    					//2.发送秒杀请求执行秒杀
+    					$.post(killUrl,{},function(result){
+    						if(result && result['success']){
+    							var killResult = result['data'];
+    							var state = killResult['state'];
+    							var stateInfo = killResult['stateInfo'];
+    							//3.显示秒杀结果
+    							node.html('<span class="label label-success">'+stateInfo+'</span>');
+    						}
+    					});
+    				});
+    				node.show();
+    			}else{
+    				//未开启秒杀
+    				var now = exposer['now'];
+    				var start = exposer['start'];
+    				var end = exposer['end'];
+    				//重新计算计时逻辑
+    				seckill.countdown(seckillId,now,start,end);
+    			}
+    		}else{
+    			console.log('result : '+result);
+    		}
+    	});
     },
     //验证手机号
     validatePhone: function (phone) {
@@ -31,9 +77,14 @@ var seckill = {
     			//时间格式
     			var format = event.strftime('秒杀倒计时：%D天 %H时 %M分 %S秒');
     			seckillBox.html(format);
+    			/*时间完成后回调事件*/
+    		}).on('finish.countdown',function(){
+    			//获取秒杀地址，控制显示逻辑，执行秒杀
+    			seckill.handleSeckill(seckillId,seckillBox);
     		});
     	}else{
     		//秒杀开始
+    		seckill.handleSeckill(seckillId,seckillBox);
     	}
     },
     //详情页秒杀逻辑
